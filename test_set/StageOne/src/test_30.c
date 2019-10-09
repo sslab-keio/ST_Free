f048c0cb87057f000a9e625a23e0bc1482a4a4d0
Keita Suzuki <keitasuzuki.park@sslab.ics.keio.ac.jp>
2019-10-09 09:54:35 +0000

    Add test case

---
+++ test_set/StageOne/src/test_30.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct testA {
+    int x;
+    int y;
+    struct testB *tB;
+};
+
+struct testB {
+    int x;
+    int y;
+};
+
+void freeA(struct testA *tA) {
+    free(tA);
+}
+
+int freeB(struct testA *tA) {
+    int err;
+    if (err < 0)
+        return -1;
+
+    free(tA->tB);
+    return 0;
+}
+
+int main()
+{
+    struct testA *tA;
+    tA = (struct testA *)malloc(sizeof(struct testA));
+    tA->tB = (struct testB *)malloc(sizeof(struct testB));
+
+    freeB(tA);
+    freeA(tA);
+
+    return 0;
+}
